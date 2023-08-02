7e2038e121b282a6f66e28259e66daec36a3ade5
Jonathan Arias <sinruidotec@gmail.com>
2023-08-02 02:43:58 -0500

    Deploy dynamic html content with DOM

--- speakers.js
+++ speakers.js
@@ -2,58 +2,58 @@ const speakers = document.querySelector(".featured__speakers-grid");
 const data = [
   {
     img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    title: "David Johnson",
+    subTitle: "Avionics Systems Engineer",
+    info: "Highly skilled in flight electronics and problem-solving, David ensures the smooth operation and safety of modern aircraft..",
     classes: "speakers__grid-item"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/speaker_01 22.png",
+    title: "Laura Martinez",
+    subTitle:"Commercial Pilot",
+    info: "Experienced and skilled, Laura's exceptional navigation abilities have earned her respect among peers in the aviation community..",
     classes: "speakers__grid-item"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/speaker_01 1.png",
+    title: "David Johnson",
+    subTitle: "Avionics Systems Engineer",
+    info: "Highly skilled in flight electronics and problem-solving, David ensures the smooth operation and safety of modern aircraft..",
     classes: "hidden-mobile"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/n1.png",
+    title: "Andrea García",
+    subTitle: "Aeronautical Engineererkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "Passionate about aircraft design and innovation, Andrea contributes significantly to cutting-edge projects in the aviation industry..",
     classes: "hidden-mobile"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/n2.png",
+    title: " Michael Smith",
+    subTitle: "Air Traffic Controller",
+    info: "Seasoned and efficient, Michael manages airspace and coordinates flights with precision..",
     classes: "hidden-mobile"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/n3.png",
+    title: "Laura Martinez",
+    subTitle:"Commercial Pilot",
+    info: "Experienced and skilled, Laura's exceptional navigation abilities have earned her respect among peers in the aviation community..",
     classes: "hidden-mobile"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/n4.png",
+    title: "Sofia Lopez",
+    subTitle: "Aeronautical Maintenance Technician.",
+    info: " Meticulous and committed to aviation safety, Sofia is an indispensable member of her maintenance team..",
     classes: "hidden-mobile"
   },
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "Capitan Delta",
-    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    img: "Assets/n5.png",
+    title: "Robert Brown",
+    subTitle: "Aviation Safety Researcher",
+    info: "Dedicated to enhancing safety protocols, Robert's work contributes to making air travel safer for passengers and crew..",
     classes: "hidden-mobile"
   },
 
