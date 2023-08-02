b108683e521a5384dba5b3f6385e6559b4eead6c
Jonathan Arias <sinruidotec@gmail.com>
2023-08-02 02:50:34 -0500

    Fix linters issues

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,21 +1,20 @@
-const barsBtn = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".lower__nav-menu");
-const closeBtn = document.querySelector(".closeMenu")
-const closeElement = document.querySelectorAll(".close-menu")
+const barsBtn = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.lower__nav-menu');
+const closeBtn = document.querySelector('.closeMenu');
+const closeElement = document.querySelectorAll('.close-menu');
 
-navMenu.classList.remove("lower__nav-menu-visible");
+navMenu.classList.remove('lower__nav-menu-visible');
 
-barsBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("lower__nav-menu-visible");
+barsBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('lower__nav-menu-visible');
 });
 
-closeBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("lower__nav-menu-visible");
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('lower__nav-menu-visible');
 });
 
-closeElement.forEach(item => {
-  item.addEventListener("click", () => {
-    navMenu.classList.toggle("lower__nav-menu-visible");
-  })
-})
-
+closeElement.forEach((item) => {
+  item.addEventListener('click', () => {
+    navMenu.classList.toggle('lower__nav-menu-visible');
+  });
+});
--- speakers.js
+++ speakers.js
@@ -1,68 +1,69 @@
-const speakers = document.querySelector(".featured__speakers-grid");
+const speakers = document.querySelector('.featured__speakers-grid');
 const data = [
   {
-    img: "Assets/speaker_01 1.svg",
-    title: "David Johnson",
-    subTitle: "Avionics Systems Engineer",
-    info: "Highly skilled in flight electronics and problem-solving, David ensures the smooth operation and safety of modern aircraft..",
-    classes: "speakers__grid-item"
+    img: 'Assets/speaker_01 1.svg',
+    title: 'David Johnson',
+    subTitle: 'Avionics Systems Engineer',
+    info: 'Highly skilled in flight electronics and problem-solving, David ensures the smooth operation and safety of modern aircraft..',
+    classes: 'speakers__grid-item',
   },
   {
-    img: "Assets/speaker_01 22.png",
-    title: "Laura Martinez",
-    subTitle:"Commercial Pilot",
+    img: 'Assets/speaker_01 22.png',
+    title: 'Laura Martinez',
+    subTitle: 'Commercial Pilot',
     info: "Experienced and skilled, Laura's exceptional navigation abilities have earned her respect among peers in the aviation community..",
-    classes: "speakers__grid-item"
+    classes: 'speakers__grid-item',
   },
   {
-    img: "Assets/speaker_01 1.png",
-    title: "David Johnson",
-    subTitle: "Avionics Systems Engineer",
-    info: "Highly skilled in flight electronics and problem-solving, David ensures the smooth operation and safety of modern aircraft..",
-    classes: "hidden-mobile"
+    img: 'Assets/speaker_01 1.png',
+    title: 'David Johnson',
+    subTitle: 'Avionics Systems Engineer',
+    info: 'Highly skilled in flight electronics and problem-solving, David ensures the smooth operation and safety of modern aircraft..',
+    classes: 'hidden-mobile',
   },
   {
-    img: "Assets/n1.png",
-    title: "Andrea García",
-    subTitle: "Aeronautical Engineererkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-    info: "Passionate about aircraft design and innovation, Andrea contributes significantly to cutting-edge projects in the aviation industry..",
-    classes: "hidden-mobile"
+    img: 'Assets/n1.png',
+    title: 'Andrea García',
+    subTitle: 'Aeronautical Engineererkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    info: 'Passionate about aircraft design and innovation, Andrea contributes significantly to cutting-edge projects in the aviation industry..',
+    classes: 'hidden-mobile',
   },
   {
-    img: "Assets/n2.png",
-    title: " Michael Smith",
-    subTitle: "Air Traffic Controller",
-    info: "Seasoned and efficient, Michael manages airspace and coordinates flights with precision..",
-    classes: "hidden-mobile"
+    img: 'Assets/n2.png',
+    title: ' Michael Smith',
+    subTitle: 'Air Traffic Controller',
+    info: 'Seasoned and efficient, Michael manages airspace and coordinates flights with precision..',
+    classes: 'hidden-mobile',
   },
   {
-    img: "Assets/n3.png",
-    title: "Laura Martinez",
-    subTitle:"Commercial Pilot",
+    img: 'Assets/n3.png',
+    title: 'Laura Martinez',
+    subTitle: 'Commercial Pilot',
     info: "Experienced and skilled, Laura's exceptional navigation abilities have earned her respect among peers in the aviation community..",
-    classes: "hidden-mobile"
+    classes: 'hidden-mobile',
   },
   {
-    img: "Assets/n4.png",
-    title: "Sofia Lopez",
-    subTitle: "Aeronautical Maintenance Technician.",
-    info: " Meticulous and committed to aviation safety, Sofia is an indispensable member of her maintenance team..",
-    classes: "hidden-mobile"
+    img: 'Assets/n4.png',
+    title: 'Sofia Lopez',
+    subTitle: 'Aeronautical Maintenance Technician.',
+    info: ' Meticulous and committed to aviation safety, Sofia is an indispensable member of her maintenance team..',
+    classes: 'hidden-mobile',
   },
   {
-    img: "Assets/n5.png",
-    title: "Robert Brown",
-    subTitle: "Aviation Safety Researcher",
+    img: 'Assets/n5.png',
+    title: 'Robert Brown',
+    subTitle: 'Aviation Safety Researcher',
     info: "Dedicated to enhancing safety protocols, Robert's work contributes to making air travel safer for passengers and crew..",
-    classes: "hidden-mobile"
+    classes: 'hidden-mobile',
   },
 
+];
 
-]
-
-function componenDynamic (data) {
+function componenDynamic(data) {
   return data.map((featured) => {
-    const {img, title, subTitle, info, classes} = featured
+    const {
+      img, title, subTitle, info, classes,
+    } = featured;
     return `
     <div class="${classes}">  
     <div>
@@ -76,8 +77,7 @@ function componenDynamic (data) {
 </div>
 </div>
     `;
-  }).join('');  
+  }).join('');
 }
 
 speakers.innerHTML = componenDynamic(data);
-
