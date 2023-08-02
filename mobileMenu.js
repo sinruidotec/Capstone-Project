eff560e9709289c82e67b60fcdaea932b93036e3
Jonathan Arias <sinruidotec@gmail.com>
2023-08-02 00:04:40 -0500

    Add logic to nav menu button

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,13 +1,21 @@
-const navToggle = document.querySelector(".nav-toggle");
+const barsBtn = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".lower__nav-menu");
-const closeBtn = document.querySelector(".menu__button-x")
+const closeBtn = document.querySelector(".closeMenu")
+const closeElement = document.querySelectorAll(".close-menu")
 
 navMenu.classList.remove("lower__nav-menu-visible");
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("lower__nav-menu-visible")
+barsBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("lower__nav-menu-visible");
 });
 
 closeBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("lower__nav-menu-visible")
+  navMenu.classList.toggle("lower__nav-menu-visible");
 });
+
+closeElement.forEach(item => {
+  item.addEventListener("click", () => {
+    navMenu.classList.toggle("lower__nav-menu-visible");
+  })
+})
+
--- speakers.js
+++ speakers.js
@@ -1,5 +1,83 @@
-const speakers = document.querySelector("#speakers");
+const speakers = document.querySelector(".featured__speakers-grid");
+const data = [
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "speakers__grid-item"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "speakers__grid-item"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "hidden-mobile"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "hidden-mobile"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "hidden-mobile"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "hidden-mobile"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "hidden-mobile"
+  },
+  {
+    img: "Assets/speaker_01 1.svg",
+    title: "Capitan Delta",
+    subTitle: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    info: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    classes: "hidden-mobile"
+  },
 
-const speakersContent = [
-  
+
 ]
+
+function componenDynamic (data) {
+  return data.map((featured) => {
+    const {img, title, subTitle, info, classes} = featured
+    return `
+    <div class="${classes}">  
+    <div>
+    <img src="${img}" alt="here goes a picture">
+</div>
+<div class="speaker__description">
+    <h4 class="speaker__description-h4">${title}</h4>
+    <h5 class="speaker__description-h5">${subTitle}</h5>
+    <hr class="speakers__hr">
+    <p class="speaker__description-p">${info}</p>
+</div>
+</div>
+    `;
+  }).join('');  
+}
+
+speakers.innerHTML = componenDynamic(data);
+
